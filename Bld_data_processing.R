852c58fd0fb20316e007b82f72f49c23e6fc1df9
Matthew Zwerling <zwerling.matthew@epa.gov>
2025-03-28 09:12:37 -0400

    Initial commit

---
+++ Bld_data_processing.R
@@ -0,0 +1,85 @@
+en_input = read.csv("bld data/energy_input.csv")
+en_price = read.csv("bld data/energy_price.csv")
+subsidy = read.csv("bld data/IRA_building_subsidy.csv")
+nonfuel_cost = read.csv("bld data/nonfuel_cost.csv")
+service_output = read.csv("bld data/service_output.csv")
+
+Value_of_consumption_i = en_input %>% 
+  group_by(scenario,subsector,Units,region) %>% 
+  summarise(X2020 = sum(X2020),
+            X2025 = sum(X2025),
+            X2030 = sum(X2030),
+            X2035 = sum(X2035)) %>% 
+  ungroup() %>% 
+  gather(year,value,5:8) %>% 
+  mutate(year = gsub("X","",year)) %>% 
+  rename(energy_input = value) %>% 
+  left_join(en_price %>% 
+              gather(year,value,5:12) %>% 
+              mutate(year = gsub("X","",year),
+                     fuel = gsub("elect_td_bld","electricity",fuel),
+                     fuel = gsub("delivered gas","gas",fuel)) %>% 
+              select(scenario,region,subsector = fuel,year,price = value)) %>% 
+  mutate(value = energy_input*price,
+         Units  = "Billions 1975$")
+  
+Value_of_consumption = Value_of_consumption_i %>%
+  left_join(Value_of_consumption_i %>% 
+              filter(scenario == "Current Policies") %>% 
+              select(subsector,region,year,cpol_value = value)) %>% 
+  mutate("PercentChangeFromCPOL" = (value/cpol_value) - 1) %>% 
+  select(-cpol_value)
+
+Change_In_Tech_Investment_i = service_output %>% 
+  group_by(scenario,region,sector,technology,Units) %>% 
+  summarise(X2020 = sum(X2020),
+            X2025 = sum(X2025),
+            X2030 = sum(X2030),
+            X2035 = sum(X2035)) %>% 
+  ungroup() %>% 
+  gather(year,value,6:9) %>% 
+  mutate(year = gsub("X","",year)) %>% 
+  left_join(nonfuel_cost %>% 
+              gather(year,cost,7:14) %>% 
+              mutate(year = gsub("X","",year)) %>% 
+              select(-Units, -scenario)) %>% 
+  mutate(invest = cost*value,
+         Units  = "Billions 1975$")
+
+Change_In_Tech_Investment = Change_In_Tech_Investment_i %>% 
+  left_join(Change_In_Tech_Investment_i %>% 
+              filter(scenario == "Current Policies") %>% 
+              select(region,sector,technology,Units,year,cpol_invest = invest)) %>% 
+  mutate("PercentChangeFromCPOL" = (invest/cpol_invest) - 1) %>% 
+  select(-cpol_invest)
+  
+  
+Value_of_Subsidy = service_output %>% 
+  group_by(scenario,region,sector,technology,Units) %>% 
+  summarise(X2020 = sum(X2020),
+            X2025 = sum(X2025),
+            X2030 = sum(X2030),
+            X2035 = sum(X2035)) %>% 
+  ungroup() %>% 
+  gather(year,value,6:9) %>% 
+  mutate(year = gsub("X","",year)) %>% 
+  filter(scenario == "ExecRollback") %>% 
+  rename(ExecRollback = value) %>% 
+  select(-scenario) %>% 
+  left_join(service_output %>% 
+              group_by(scenario,region,sector,technology,Units) %>% 
+              summarise(X2020 = sum(X2020),
+                        X2025 = sum(X2025),
+                        X2030 = sum(X2030),
+                        X2035 = sum(X2035)) %>% 
+              ungroup() %>% 
+              gather(year,value,6:9) %>% 
+              mutate(year = gsub("X","",year)) %>% 
+              filter(scenario == "FedRollback") %>% 
+              rename(FedRollback = value) %>% 
+              select(-scenario)) %>% 
+  mutate(IRAEffect = ExecRollback-FedRollback) %>% 
+  left_join(subsidy %>% 
+              select(-subsector, -Units)) %>% 
+  mutate(ValueOfSubsidy = subsidy*IRAEffect) %>% 
+  summarise(ValueOfSubsidy = sum(ValueOfSubsidy,na.rm =TRUE))
