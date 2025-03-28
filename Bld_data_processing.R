31577509ad4860b92a0584f677a44d47904d97f5
Matthew Zwerling <zwerling.matthew@epa.gov>
2025-03-28 16:20:15 -0400

    Update to data processing, and processed data

--- Bld_data_processing.R
+++ Bld_data_processing.R
@@ -5,6 +5,8 @@ en_price = read.csv("bld data/energy_price.csv")
 subsidy = read.csv("bld data/IRA_building_subsidy.csv")
 nonfuel_cost = read.csv("bld data/nonfuel_cost.csv")
 service_output = read.csv("bld data/service_output.csv")
+state_map = read.csv("Mapping_state_USREP.csv") %>% 
+  rename(region = state, USREP_region = r)
 
 Value_of_consumption_i = en_input %>% 
   group_by(scenario,subsector,Units,region) %>% 
@@ -23,14 +25,20 @@ Value_of_consumption_i = en_input %>%
                      fuel = gsub("delivered gas","gas",fuel)) %>% 
               select(scenario,region,subsector = fuel,year,price = value)) %>% 
   mutate(value = energy_input*price,
-         Units  = "Billions 1975$")
+         Units  = "Billions 1975$") %>% 
+  left_join(state_map) %>% 
+  group_by(scenario, subsector,Units,USREP_region,year) %>% 
+  summarise(value= sum(value)) %>% 
+  ungroup()
   
 Value_of_consumption = Value_of_consumption_i %>%
   left_join(Value_of_consumption_i %>% 
               filter(scenario == "Current Policies") %>% 
-              select(subsector,region,year,cpol_value = value)) %>% 
+              select(subsector,USREP_region,year,cpol_value = value)) %>% 
   mutate("PercentChangeFromCPOL" = (value/cpol_value) - 1) %>% 
   select(-cpol_value)
+
+write.csv(Value_of_consumption,"Value_of_consumption.csv")
 
 Change_In_Tech_Investment_i = service_output %>% 
   group_by(scenario,region,sector,technology,Units) %>% 
@@ -46,15 +54,20 @@ Change_In_Tech_Investment_i = service_output %>%
               mutate(year = gsub("X","",year)) %>% 
               select(-Units, -scenario)) %>% 
   mutate(invest = cost*value,
-         Units  = "Billions 1975$")
+         Units  = "Billions 1975$") %>% 
+  left_join(state_map) %>% 
+  group_by(scenario, sector, technology,subsector,Units,USREP_region,year) %>% 
+  summarise(invest= sum(invest)) %>% 
+  ungroup()
 
 Change_In_Tech_Investment = Change_In_Tech_Investment_i %>% 
   left_join(Change_In_Tech_Investment_i %>% 
               filter(scenario == "Current Policies") %>% 
-              select(region,sector,technology,Units,year,cpol_invest = invest)) %>% 
+              select(USREP_region,sector,technology,Units,year,cpol_invest = invest)) %>% 
   mutate("PercentChangeFromCPOL" = (invest/cpol_invest) - 1) %>% 
   select(-cpol_invest)
   
+write.csv(Change_In_Tech_Investment,"Change_In_Tech_Investment.csv")
   
 Value_of_Subsidy = service_output %>% 
   group_by(scenario,region,sector,technology,Units) %>% 
@@ -85,3 +98,5 @@ Value_of_Subsidy = service_output %>%
               select(-subsector, -Units)) %>% 
   mutate(ValueOfSubsidy = subsidy*IRAEffect) %>% 
   summarise(ValueOfSubsidy = sum(ValueOfSubsidy,na.rm =TRUE))
+
+write.csv(Value_of_Subsidy,"Value_of_Subsidy.csv")
